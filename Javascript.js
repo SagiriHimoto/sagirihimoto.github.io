af36cdbf73a6278b63c6750b69eb14c9bb78e299
SagiriHimoto <79280544+SagiriHimoto@users.noreply.github.com>
2022-05-29 19:36:25 +0300

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,22 @@
+/* Scroll progressbar */
+/* window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  var appear = 0;
+	if(scrolled>0){
+		appear = 8
+	}
+  document.getElementById("barcontainer").style.height = appear + "px";
+  document.getElementById("myBar").style.height = appear + "px";
+  document.getElementById("myBar").style.width = scrolled + "%";
+} */
+var Sagiri =
+{
+    pfp : "https://i.stack.imgur.com/vJscc.png",
+    statusmuted : 18,
+    linklink : "CSE",
+    linktext : 90
+};
