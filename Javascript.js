78afe004f9cae3ec36f7718cd5af5a2594a08690
SagiriHimoto <sagirhimoto@gmail.com>
2023-04-13 16:39:54 +0300

    yes

--- Javascript.js
+++ Javascript.js
@@ -2,7 +2,12 @@
 function createHeaderAndFooter() {
     var header="<div class=\"navigationbar\" id=\"navbarshrinky\">\r\n<a href=\"404.html\"><div id=\"wholetitle\" class=\"titlish\"><img class=\"imglogo44\" src=\"assets\/AzureChiveLogo64x64.png\"><\/img> AzureChive<\/div>\r\n<a id=\"helpa\" class=\"inactive hoverablea\" href=\"404.html\"><i class=\"fa-solid fa-circle-question\"><\/i> Help<\/a>\r\n<a id=\"abouta\" class=\"inactive hoverablea\" href=\"404.html\"><i class=\"fa-solid fa-circle-info\"><\/i> About<\/a>\r\n<a id=\"homea\" class=\"inactive hoverablea\" href=\"404.html\"><i class=\"fa-solid fa-house\"><\/i> Home<\/a>\r\n\t\t<div class=\"progress-container\" id=\"barcontainer\">\r\n\t\t<div class=\"progress-bar\" id=\"myBar\"><\/div>\r\n\t<\/div>\r\n<\/div>";
     var footer="<div class=\"FooterPart\"><center>Copyright &copy; 2018 \"Sagiri&amp;Makigiri\" All Rights reserved.<\/center><\/div>";
-    document.body.innerHTML=header+document.body.innerHTML+footer;
+	document.body.innerHTML = header + document.body.innerHTML + footer;
+	if (document.getElementById("skin") != null) {
+		var idElem = document.getElementById("SkinParams");
+		var skinsParamsDiv = "<div>PARAMETERS GO HERE:<br/>" + Param1 + "</div>"
+		idElem.innerHTML = skinsParamsDiv
+	}
 }
 window.addEventListener("load", createHeaderAndFooter);
 
