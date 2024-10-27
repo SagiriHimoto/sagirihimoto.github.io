931ed468a34995b85edda43c1ee16634b2f9d0a6
SagiriHimoto <sagirhimoto@gmail.com>
2024-10-27 16:23:29 +0200

    New Index

--- Javascript.js
+++ Javascript.js
@@ -1,24 +1,15 @@
 ﻿/* "https://i.stack.imgur.com/vJscc.png" */
-
+let allowmobile = true
 function createHeaderAndFooter() {
-    var header="<div class='navigationbar'><img class='imglogo44' src='assets/AzureChiveLogo64x64.png'></img> AzureChive</div>";
+	var header='<nav class="navbar navbar-expand-md bg-dark py-3" data-bs-theme="dark"><div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><img class="imglogo44" src="assets/AzureChiveLogo64x64.png"></img></span><span>AzureChive</span></a><button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button><div id="navcol-5" class="collapse navbar-collapse"><ul class="navbar-nav ms-auto"><li class="nav-item"><a class="nav-link active" href="#">Home</a></li></ul><a class="btn btn-primary ms-md-2" role="button" href="#">Button</a></div></div></nav>'
+    /* var header="<div class='navigationbar'><img class='imglogo44' src='assets/AzureChiveLogo64x64.png'></img> AzureChive</div>"; */
     var stylezimport = '<link rel="stylesheet" href="styles/styles.css"/>< link rel="stylesheet" href="styles/flavors.css" /><link href="styles/btn.css" rel="stylesheet" /><link href="styles/cursors.css" rel="stylesheet"/>';
-    var footer = "<div class='FooterPart'><center style='margin-top:15px;'><button class='btntwitt' onclick='location.href =\"https://twitter.com/intent/follow?screen_name=SagiriHimoto\";'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='#fff' viewBox='0 0 24 24'><path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'/></button><br/>Copyright &copy; 2018 \"Sagiri Himoto\" All Rights reserved.<br/><a>Check our policies</a><br/><br/></center></div>";
+    var footer = "<footer class=\"text-center bg-dark\"><center style='margin-top:15px;'><button class='btntwitt' onclick='location.href =\"https://twitter.com/intent/follow?screen_name=SagiriHimoto\";'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='#fff' viewBox='0 0 24 24'><path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'/></button><br/><span style=\"color:#fff;\">Copyright &copy; 2018 \"Sagiri Himoto\" All Rights reserved.<br/><a>Check our policies</a></span></center></footer>";
     document.body.innerHTML = header + document.body.innerHTML + footer;
-    if (window.innerWidth > 535) { document.body.style.opacity = "1" } else { document.body.innerHTML = "<center><div style='margin-top: 150px; '><span style='font-size: 80px; color: black; background-color: white; padding: 8px; border-radius: 5px;'>Get a normal device brother</span></div></center>"; document.body.style.opacity = "1"; };
+    if (window.innerWidth > 535) { document.body.style.opacity = "1" } else if (allowmobile){ document.body.style.opacity = "1" } else { document.body.innerHTML = "<center><div style='margin-top: 150px; '><span style='font-size: 80px; color: black; background-color: white; padding: 8px; border-radius: 5px;'>Get a normal device brother</span></div></center>"; document.body.style.opacity = "1"; };
     document.head.innerHTML = stylezimport + document.head.innerHTML;
     }
 window.addEventListener("load", createHeaderAndFooter);
-
-
-const CP = require('@taufik-nurrohman/color-picker').default;
-
-const picker = new CP(document.querySelector('input'));
-
-picker.on('change', function (r, g, b, a) {
-    this.source.value = this.color(r, g, b, a);
-});
-
 
 /* LOG */
 /* LOG */
